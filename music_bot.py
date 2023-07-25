6fccc20af0e4307f93f89df1ae8537b0bceb0f50
Speedy <91589360+Darklightning2008@users.noreply.github.com>
2023-07-25 20:54:29 +0530

    Create music_bot.py

---
+++ music_bot.py
@@ -0,0 +1,10 @@
+import logging
+from telegram.ext import Updater, CommandHandler
+
+# Set up logging (optional, but useful for debugging)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+# Replace 'YOUR_BOT_TOKEN' with the token you obtained from BotFather
+TOKEN = '6228213035:AAG1oLG3aP4f3Axb_6tHuLERhffK9Uhbt6I'
+updater = Updater(TOKEN, use_context=True)
+dispatcher = updater.dispatcher
